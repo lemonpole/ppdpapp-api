1774cfaf3ae85521a22ee9041cac00586b707b1b
Steve Rivera <srivera.x1@gmail.com>
2014-10-04 20:24:17 -0400

    switched compile method to WAR output instead of using spring's embedded tomcat instance.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,13 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
+@Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
