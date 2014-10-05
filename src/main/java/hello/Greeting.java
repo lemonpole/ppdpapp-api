ebc74daa8134184cbb46ce93fb843f3d93fb1e55
Steve Rivera <srivera.x1@gmail.com>
2014-10-04 21:02:46 -0400

    removed spring getting started files.

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Greeting {
+	private final String content;
+	
+	public Greeting(String content){
+		this.content = content;	
+	}
+	public String getContent(){
+		return this.content;	
+	}
+}
