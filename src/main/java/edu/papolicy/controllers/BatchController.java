9fdd3393e28e9b55c802f74ac7ef8ab219847a24
Steve Rivera <srivera.x1@gmail.com>
2014-10-21 22:40:19 -0400

    added batches/user relationship.

--- src/main/java/edu/papolicy/controllers/BatchController.java
+++ src/main/java/edu/papolicy/controllers/BatchController.java
@@ -2,10 +2,11 @@ package edu.papolicy.controllers;
 
 import edu.papolicy.daos.BatchDAO;
 import edu.papolicy.models.Batch;
+import edu.papolicy.models.User;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/batches")
@@ -14,11 +15,22 @@ public class BatchController {
 	private BatchDAO batchDAO;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Batch> getBatches(){ return batchDAO.list(); }
+	public List<Batch> getBatches(){
+		return batchDAO.list();
+	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
-	public Batch getBatch(@PathVariable int id){ return batchDAO.find(id); }
+	public Batch getBatch(@PathVariable int id){
+		return batchDAO.find(id);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}/users")
+	public List<User> getBatchUsers(@PathVariable int id){
+		return batchDAO.findUsers(id);
+	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public Batch postBatch(@RequestBody Batch batchObj){ return batchDAO.save(batchObj); }
+	public Batch postBatch(@RequestBody Batch batchObj){
+		return batchDAO.save(batchObj);
+	}
 }
--- src/main/java/edu/papolicy/daos/UserDAOImpl.java
+++ src/main/java/edu/papolicy/daos/UserDAOImpl.java
@@ -1,12 +1,12 @@
 package edu.papolicy.daos;
 
 import edu.papolicy.models.User;
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 public class UserDAOImpl implements UserDAO {
     @Autowired
