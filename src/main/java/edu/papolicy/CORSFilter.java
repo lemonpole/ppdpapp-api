c276fc025cef97b052010474b8be4f152506491f
Steve Rivera <srivera.x1@gmail.com>
2014-11-04 21:11:11 -0500

    Added auth request header for CORS.

--- src/main/java/edu/papolicy/CORSFilter.java
+++ src/main/java/edu/papolicy/CORSFilter.java
@@ -12,8 +12,8 @@ public class CORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		chain.doFilter(req, res);
 	}
 	public void init(FilterConfig filterConfig) {}
