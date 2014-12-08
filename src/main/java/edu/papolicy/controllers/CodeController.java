6add23e72d20c82803b82da25544a908f6f6cc08
Steven <stevenheyen4@gmail.com>
2014-12-08 02:11:26 -0500

    changed code controller to return responseEntity

--- src/main/java/edu/papolicy/controllers/CodeController.java
+++ src/main/java/edu/papolicy/controllers/CodeController.java
@@ -1,11 +1,14 @@
 package edu.papolicy.controllers;
 
 import edu.papolicy.daos.CodeDAO;
+import edu.papolicy.models.Batch;
 import edu.papolicy.models.Code;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,11 +18,14 @@ public class CodeController {
     private CodeDAO codeDAO;
 
     @RequestMapping(method=RequestMethod.GET, value="/{tableName}")
-    public List<Object> getCodes(@PathVariable String tableName){ return codeDAO.list(tableName); }
+    public ResponseEntity<List<Object>> getCodes(@PathVariable String tableName){
+        return new ResponseEntity<List<Object>>(codeDAO.list(tableName), HttpStatus.OK);}
 
 	@RequestMapping(method=RequestMethod.GET, value="/{tableName}/{id}")
-	public Object getCode(@PathVariable String tableName, @PathVariable int id){ return codeDAO.find(tableName,id); }
+	public ResponseEntity<Object> getCode(@PathVariable String tableName, @PathVariable int id){
+        return new ResponseEntity<Object>(codeDAO.find(tableName,id), HttpStatus.OK);}
 
     @RequestMapping(method=RequestMethod.GET, value="/{tableName}/search/")
-    public Object getCodeSearch(@PathVariable String tableName, @RequestParam(value="query") String search){ return codeDAO.findSearch(tableName,search); }
+    public ResponseEntity<List<Object>> getCodeSearch(@PathVariable String tableName, @RequestParam(value="query") String search){
+        return new ResponseEntity<List<Object>>(codeDAO.findSearch(tableName,search), HttpStatus.OK); }
 }
